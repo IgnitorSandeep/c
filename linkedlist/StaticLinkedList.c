ea6272d1131a7aac71a0b2d3140f384ced90af6f
Sandeep Singh <sandeepsingh1009@gmail.com>
2014-12-21 18:35:34 +0530

    linked list

--- linkedlist/StaticLinkedList.c
+++ linkedlist/StaticLinkedList.c
@@ -6,7 +6,8 @@ struct node
 };
 
 
-
+//Return the address of second last node
+//Using temp variable
 struct node* secondLastNodeAddr(struct node *s)
 {
 
@@ -27,6 +28,75 @@ struct node* secondLastNodeAddr(struct node *s)
     }
     return p;
 }
+
+//Return the address of second last node
+//without using temp variable
+struct node* secondLastNodeAddr2(struct node *s)
+{
+
+    if(s==NULL)
+    {
+        printf("Linked List is empty.\n");
+        return s;
+    }
+    if(s->next==NULL)
+    {
+        printf("Linked List has single element.\n");
+        return s;
+    }
+    while(s->next->next!=NULL)
+    {
+        s=s->next;
+    }
+    return s;
+}
+
+struct node* findMiddle(struct node *s)
+{
+    struct node *sp,*fp;
+    sp=fp=s;
+    if(fp==NULL)
+    {
+        printf("linked list is empty.");
+        return fp;
+    }
+    if(fp->next==NULL)
+    {
+         printf("linked list has only one element.");
+         return fp;
+    }
+    while(fp->next!=NULL&&fp->next->next!=null)
+    {
+        sp=sp->next;
+        fp=fp->next->next;
+    }
+
+    return sp;
+}
+//return address of the node which contain data x.
+struct node* findElementX(struct node *s,int x)
+{
+    if(s==NULL)
+    {
+        printf("Linked list is empty.\n");
+        return s;
+    }
+    while(s->data!=x&&s->next!=NULL)
+    {
+        s=s->next;
+
+    }
+    if(s->data==x)
+    {
+       return s;
+       return s;
+    }
+    else
+    {
+          printf("data not present in the list.\n");
+    }
+
+};
 
 int main(void)
 {
@@ -52,6 +122,16 @@ int main(void)
     //fetching second last node data
     p = secondLastNodeAddr(s);
     printf("\n\n2nd last element : %d.\n",p->data);
+
+    //fetching second last node data
+    p = secondLastNodeAddr2(s);
+    printf("\n\n2nd last element : %d.\n",p->data);
+
+    //fetching second last node data
+    p = findElementX(s,40);
+    printf("\n\n Address of node with data 40 is %u.\n",p);
+    printf("\n\n data is %d.\n",p->data);
+
 
     return 0;
 
