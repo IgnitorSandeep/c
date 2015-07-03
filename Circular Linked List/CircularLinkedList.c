20f7e52755b7937cb1493b5516c65bc0b606364a
Sandeep Singh <sandeepsingh1009@gmail.com>
2015-07-04 00:44:36 +0530

    Resolved the Segmentation Fault

--- Circular Linked List/CircularLinkedList.c
+++ Circular Linked List/CircularLinkedList.c
@@ -13,11 +13,11 @@ int circularListLength(struct CircularListNode *head) {
     if(current==NULL) {
         return 0;
     }
-
-    while(current->next!=head) {
+    do {
         current = current->next;
         length++;
     }
+    while(current!=head);
 
     return length;
 }
@@ -25,13 +25,18 @@ int circularListLength(struct CircularListNode *head) {
 void printList(struct CircularListNode *head) {
     struct CircularListNode *current = head;
 
-    if(head==NULL)
+    if(head==NULL) {
+        printf("NULL\n");
         return;
+    }
 
-    while(current->next!=head) {
-        printf("%d",current->data);
+    printf("head->");
+    do {
+        printf("%d->",current->data);
         current = current->next;
     }
+    while(current!=head);
+    printf("head\n");
 }
 
 struct CircularListNode* insertAtEnd(struct CircularListNode *head,int data) {
@@ -46,14 +51,16 @@ struct CircularListNode* insertAtEnd(struct CircularListNode *head,int data) {
     newNode->data = data;
     newNode->next = newNode;
 
-    while(current->next!=head)
-        current = current->next;
+
 
     if(head==NULL) {
         head=newNode;
         return head;
     }
     else {
+        while(current->next!=head)
+            current = current->next;
+
         newNode->next = head;
         current->next = newNode;
         return head;
@@ -72,14 +79,14 @@ struct CircularListNode* insertAtBegin(struct CircularListNode *head, int data) 
     newNode->data = data;
     newNode->next = newNode;
 
-    while(current->next!=head) {
-        current = current->next;
-    }
-
     if(head == NULL) {
         head = newNode;
     }
     else {
+
+        while(current->next!=head)
+            current = current->next;
+
         newNode->next = head;
         current->next = newNode;
         head = newNode;
@@ -102,15 +109,42 @@ struct CircularListNode* deleteLastNode(struct CircularListNode *head) {
     }
 
     if(current->next==head) {
+        //printf("[DEBUG] Current Next is Head\n");
         temp->next = current->next;
         free(current);
         current = NULL;
+        return head;
+    }
+}
+
+struct CircularListNode* deleteFirstNode(struct CircularListNode *head) {
+    struct CircularListNode *current = head;
+    struct CircularListNode *temp;
+
+    if(current ==  NULL) {
+        printf("List is empty.");
         return;
+    }
+
+    while(current->next!=head) {
+        //temp = current;
+        current = current->next;
+    }
+
+    if(current->next==head) {
+        //printf("[DEBUG] Current Next is Head\n");
+        current->next = head->next;
+        free(head);
+        head = current->next;
+        return head;
     }
 }
 
 int main(void) {
     struct CircularListNode *start = NULL;
+
+    printList(start);
+    printf("Current List Length : %d\n",circularListLength(start));
 
     start = insertAtBegin(start,1);
     start = insertAtBegin(start,2);
@@ -118,8 +152,33 @@ int main(void) {
     start = insertAtBegin(start,4);
     start = insertAtBegin(start,5);
 
-    //Printing the circular list
+    printf("\n");
+    printf("After inserting 1, 2, 3, 4, 5 at begin.\n");
     printList(start);
+    printf("Current List Length : %d\n",circularListLength(start));
+
+    start = insertAtEnd(start,2);
+    start = insertAtEnd(start,3);
+    start = insertAtEnd(start,4);
+    start = insertAtEnd(start,5);
+
+    printf("\n");
+    printf("After inserting 2, 3, 4, 5 at end.\n");
+    printList(start);
+    printf("Current List Length : %d\n",circularListLength(start));
+
+    printf("\n");
+    printf("After Last Node Deletion.\n");
+    start = deleteLastNode(start);
+    printList(start);
+    printf("Current List Length : %d\n",circularListLength(start));
+
+    printf("\n");
+    printf("After First Node Deletion.\n");
+    start = deleteFirstNode(start);
+    printList(start);
+    printf("Current List Length : %d\n",circularListLength(start));
+
 
     return 0;
 }
