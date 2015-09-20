70d7e7e41ad61053c8c8b6b2fa78140501d2b143
Sandeep Singh <sandeepsingh1009@gmail.com>
2015-09-21 04:03:54 +0530

    Removing Tree

--- Linked List/Doubly Linked List/DoublyLinkedList.c
+++ Linked List/Doubly Linked List/DoublyLinkedList.c
@@ -7,20 +7,141 @@ struct DoublyListNode {
     struct DoublyListNode *next;
 };
 
+
+/**
+Doubly linked list node
+*/
+struct dnode
+{
+    struct dnode *prev;
+    int data;
+    struct dnode *next;
+
+};
+
+
+/**
+Insert a node in sorted doubly linked list
+*/
+struct dnode* SortedInsert(struct dnode *head,int data)
+{
+    struct dnode *temp,*current;
+    temp=(struct dnode*)malloc(sizeof(struct dnode));
+    temp->data = data;
+    temp->next = NULL;
+    temp->prev = NULL;
+
+    if(head==NULL)
+    {
+        head=temp;
+        //printf("%d",head->data);
+        return head;
+    }
+    if(temp->data<=head->data)
+    {
+        temp->next=head;
+        head->prev=temp;
+        head=temp;
+        return head;
+    }
+    current=head;
+    while(current->next!=NULL)
+    {
+       if(temp->data<=current->data)
+       {
+           current->prev->next =temp;
+           temp->next = current;
+           temp->prev = current->prev;
+           current->prev = temp;
+           return head;
+       }
+       current = current->next;
+    }
+    if(temp->data<=current->data)
+    {
+       current->prev->next=temp;
+       temp->next=current;
+       temp->prev=current->prev;
+       current->prev=temp;
+       return head;
+    }
+    current->next=temp;
+    temp->prev=current;
+
+    return head;
+}
+
+/**
+To reverse a doubly linked
+*/
+struct dnode* reverseDoublyList(struct dnode *head)
+{
+    struct dnode *p,*q,*r;
+    p=q=NULL;
+    r=head;
+    if(r==NULL||r->next==NULL)
+    {
+        return head;
+    }
+
+    while(r->next!=NULL)
+    {
+        p=q;
+        q=r;
+
+        r=r->next;
+        q->prev = q->next;
+        q->next = p;
+
+    }
+    //last node
+    if(r->next==NULL)
+    {
+        r->next=r->prev;
+        r->prev=NULL;
+        head = r;
+        return head;
+    }
+    return head;
+}
+
+/**
+To print all nodes of the doubly linked list
+*/
+void printDoublyList(struct dnode *head)
+{
+    while(head!=NULL)
+    {
+        printf("%d  ",head->data);
+        head=head->next;
+    }
+}
+
+
 struct DoublyListNode* insertAtBegin(struct DoublyListNode *head,int data) {
 
     struct DoublyListNode *current = head;
     struct DoublyListNode *newNode = (struct DoublyListNode*)malloc(sizeof(struct DoublyListNode));
     newNode->data = data;
     newNode->prev = NULL;
+    newNode->next = head;
+
 
     if(head == NULL) {
         head = newNode;
+
+         return head;
     }
     else {
-        newNode->next = head;
-        head = newNode;
+         head ->prev =newNode;
+         head = newNode;
+
+         return head;
     }
+
+
+
+
 }
 
 struct DoublyListNode* insertAtEnd(struct DoublyListNode *head,int data) {
@@ -199,6 +320,28 @@ int main(void) {
     printList(head);
     printf("Length of the list is %d.\n",getCount(head));
 
+
+    //Inserting into a doubly linked list
+    struct dnode *dlist=NULL;
+    int arr3[]={2,4,5,6,6,7,9};
+    int size3=sizeof(arr3)/sizeof(arr3[0]);
+    int l;
+    for(l=0;l<size3;l++)
+    {
+        dlist=SortedInsert(dlist,arr3[l]);
+    }
+    printf("\nDoubly Linked List contains :\n");
+    printDoublyList(dlist);
+    printf("\n\n");
+
+    //Reversing the doubly linked list
+    struct dnode *reverseDoubleList=NULL;
+    reverseDoubleList=reverseDoublyList(dlist);
+    printf("\nReverse of Doubly Linked List contains :\n");
+    printDoublyList(reverseDoubleList);
+    printf("\n\n");
+
+
+
     return 0;
 }
-
